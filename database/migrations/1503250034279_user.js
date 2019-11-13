df1523155a1b684697e8f0cd24e2d3c242e0f476
rmuliterno <renan-mf1@hotmail.com>
2019-11-13 14:14:43 -0300

    Password recover

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -9,7 +9,9 @@ class UserSchema extends Schema {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
+	  table.string('password', 60).notNullable()
+	  table.string('token')
+	  table.timestamp('token_created_at')
       table.timestamps()
     })
   }
