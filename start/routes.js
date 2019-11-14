79b2bb6d945314f17459aab02befcba0b771f8a7
rmuliterno <renan-mf1@hotmail.com>
2019-11-14 11:18:13 -0300

    file upload

--- start/routes.js
+++ start/routes.js
@@ -6,3 +6,5 @@ Route.post('sessions', 'SessionController.store')
 
 Route.post('password', 'ForgotPasswordController.store')
 Route.put('password', 'ForgotPasswordController.update')
+
+Route.post('files', 'FileController.store')
